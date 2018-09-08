2856f891940448d915a6ab3ca34a991e273f2247
Jackson Liu <416883813@qq.com>
2018-09-08 23:15:43 +0800

    add type

--- src/types.js
+++ src/types.js
@@ -1,7 +1,30 @@
 // @flow
+import Service from './Service'
 
 export type typeServiceCenter = {
-    register: () => Boolean,
-    unregister: () => Boolean,
-    subscribe: () => Function
+    registerService: Function,
+    subscribe: (Function) => Function,
+    dispatch: Object,
+}
+
+export type typeServiceMap = {
+    [key: string]: Service
+}
+
+export type typeCreateStoreParam = {
+    serviceMap: typeServiceMap
+}
+
+export type typeCreateServiceParam<T> = {
+    state: T,
+    onInit: Function,
+    methods: {
+        [name: string]: (dispatch: Object, getState: Function ) => any
+    }
+}
+
+export interface typeService {
+    getState: Function,
+    setState: Function,
+    [name: string]: Function
 }
