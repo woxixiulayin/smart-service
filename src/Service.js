59bbd8a09609927e65bc3f9e16f9f11455d2ed36
Jackson Liu <416883813@qq.com>
2018-09-08 23:15:17 +0800

    add service

--- src/Service.js
+++ src/Service.js
@@ -1,31 +1,86 @@
 // @flow
-import assert from './utils/assert'
+const log = console.log
+const serviceInstances = []
 
-class Service <A> {
-    // 内部保存的数据
-    state: A
-    listeners: Array<Function>
+let serviceState = {}
 
+const getServiceState = () => serviceState
+
+class Service<T> {
     constructor({
         state
     }: {
-        state: A
+        state: T
     }) {
-        this.state = {}
+        this.displayName = this.constructor.name
+        if (serviceInstances[this.displayName]) {
+            throw new Error(`can not create same service with name ${this.displayName}`)
+        }
+
+        serviceInstances[this.displayName] = this
+
+        this._state = {}
+        this._listeners = []
+        this.setState(state)
     }
 
-    getState(): A {
-        return this.state
+    getState():T {
+        return this._state
     }
 
-    // 纯函数，无副作用，只有setState才能修改service状态
-    setState(newState: A) {
-        this.state = {
-            ...this.state,
-            ...newState,
+    setState(updater) {
+        const preState = this._state
+
+        if (!serviceInstances[this.displayName]) {  
+            throw new Error(`can not setState when service is not in list`)
+        }
+
+        if (typeof updater === 'function') {
+            this._state = {
+                ...preState,
+                ...updater(preState)
+            }
+        } else {
+            this._state = {
+                ...preState,
+                ...updater
+            }
+        }
+
+        this.onStateChange(preState)
+    }
+
+
+    onStateChange(preState) {
+        log(`service ${this.displayName} change state from`, preState, 'to', this._state)
+        const preServiceState = serviceState
+        
+        serviceState = {
+            ...preServiceState,
+            [this.displayName]: this._state
+        }
+
+        log(`serviceState changed from`, preServiceState, 'to', serviceState)
+
+        const currentListeners = this._listeners.slice()
+        for (const listener of currentListeners) {
+            listener()
         }
     }
 
+    subscribe(listener: Function) {
+
+        this._listeners.push(listener)
+
+        function unsubscribe(){
+            this._listeners.splice(this._listeners.indexOf[listener], 1)
+        }
+
+        return unsubscribe
+    }
 }
 
-export default Service
+export {
+    Service,
+    getServiceState,
+}
