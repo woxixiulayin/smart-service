b504a6c4cca7b2d0af6091b8ebc6bf494ac36b67
Jackson Liu <416883813@qq.com>
2018-09-14 16:48:07 +0800

    add default mapState

--- src/withService.js
+++ src/withService.js
@@ -4,7 +4,14 @@ import * as React from 'react'
 import Service from './Service'
 import connectService from './connectService'
 
-const withService = <T>(ServiceClass: Class<Service<T>>, mapState: (state: T) => any) => {
+function noop() {}
+
+/**
+ * inject service instance into component
+ * @param {Class<Service<T>} ServiceClass the service Class inject into component
+ * @param {?(state: T, ownProps?: any) => any} mapState use as mapStateToProps in react-redux
+ */
+const withService = <T>(ServiceClass: Class<Service<T>>, mapState: (state: T, ownProps?: any) => any = noop) => {
 
     const serviceInstance = Service.getServiceInstance(ServiceClass) || Service.registerService(ServiceClass)
 
